84a7cc29c22ed4fd9a31359a35870323b741f016
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-09-25 13:27:37 +0700

    make the project card correctly responsive

--- components/Project.jsx
+++ components/Project.jsx
@@ -16,7 +16,7 @@ export default function Project({
 }) {
   return (
     <div className="relative w-full max-w-3xl rounded-4xl bg-gray backdrop-blur-sm overflow-hidden">
-      <div className="relative w-full xl:h-100 sm:h-52 aspect-[16/9] overflow-hidden">
+      <div className="relative w-full sm:h-52 aspect-[16/9] overflow-hidden">
         <Image
           src={`/${img}`}
           fill
@@ -26,33 +26,23 @@ export default function Project({
         />
       </div>
 
-      <div className="relative rounded-b-4xl max-h-50 p-6 text-white bg-white/10 backdrop-blur-3xl">
+      <div className="relative rounded-b-4xl h-fit max-h-50 p-6 text-white bg-white/10 backdrop-blur-3xl">
         <h2 className="secondary-topic-text">{name}</h2>
         <div className="flex justify-between items-start ">
-          <div className="flex flex-wrap gap-y-2">
+          <div className="flex flex-wrap items-center gap-y-2">
             {stack.map((tech, i) => (
               <span
                 key={i}
                 className={[
-                  'font-semibold mr-2 rounded-2xl bg-white/40 backdrop-blur-md px-3 py-1',
-                  i >= 3 ? 'invisible sm:visible' : '',
+                  'font-semibold mr-2 rounded-2xl bg-white/40 backdrop-blur-md px-3 py-1 text-sm md:text-base',
+                  i >= 3 ? 'hidden lg:inline-flex' : '',
                 ].join(' ')}
               >
                 {tech}
               </span>
             ))}
           </div>
-          <div className="flex items-center gap-4">
-            <a
-              href={link}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={`Try ${name}'s demo`}
-              className="bg-green-600 pl-4 pr-3 py-2 rounded-xl font-bold inline-flex text-nowrap items-center gap-0.5 hover:-translate-y-1 active:translate-y-1 transition-transform duration-150 ease-in-out caret-transparent"
-            >
-              Try now!
-              <ExternalLinkIcon />
-            </a>
+          <div className="flex justify-center gap-4 text-xl max-w-[40%]">
             <a
               href={github}
               target="_blank"
@@ -60,6 +50,18 @@ export default function Project({
               aria-label="view github's repo"
             >
               <GithubIcon className="w-10 h-10 hover:text-black transform-all duration-150" />
+            </a>
+            <a
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label={`Try ${name}'s demo`}
+              className="bg-green-600 pl-4 pr-3 py-2 rounded-xl inline-flex items-center gap-0.5 hover:-translate-y-1 active:translate-y-1 transition-transform duration-150 ease-in-out caret-transparent"
+            >
+              <span className="hidden lg:inline-flex font-semibold text-nowrap mx-1">
+                Try now!
+              </span>
+              <ExternalLinkIcon />
             </a>
           </div>
         </div>
