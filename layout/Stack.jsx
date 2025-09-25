7ac19d3dfb920b2a885c23c4dfb10405371e2230
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-09-25 12:42:33 +0700

    change size of about me section on desktop viewport and add breakpoint for ui in tech stack section

--- layout/Stack.jsx
+++ layout/Stack.jsx
@@ -22,7 +22,7 @@ const Stack = () => {
     <Section>
       <div className="relative flex flex-col">
         <h2 className="topic-text">Tech Stack</h2>
-        <ul className="relative grid grid-cols-[repeat(auto-fit,minmax(15rem,1fr))] min-w-[80vw] justify-start max-w-[90vw] lg:max-w-[80vw] gap-x-4 md:gap-x-8 gap-y-4 lg:gap-y-8">
+        <ul className="relative grid grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] lg:grid-cols-[repeat(auto-fit,minmax(15rem,1fr))] min-w-[80vw] justify-start max-w-[90vw] lg:max-w-[80vw] gap-x-4 md:gap-x-8 gap-y-4 lg:gap-y-8">
           {stacks.map((tech) => (
             <TechStack tech={tech} key={tech} />
           ))}
